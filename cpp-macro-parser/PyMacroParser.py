cecbddb516edfd6f73b228e8d200a0a77322963e
zoloypzuo <910181598@qq.com>
2019-12-22 17:48:46 +0800

    Update PyMacroParser.py

--- cpp-macro-parser/PyMacroParser.py
+++ cpp-macro-parser/PyMacroParser.py
@@ -21,7 +21,7 @@ from enum import Enum, auto
 
 # region common
 def readall(path):
-    with open(path, 'r') as f:
+    with open(path, 'r', encoding='utf8') as f:
         return f.read()
 
 
@@ -59,8 +59,6 @@ class TokenKind(Enum):
     Sep_LCurly = auto()  # {
     Sep_RCurly = auto()  # }
     Sep_Comma = auto()  # ,
-    Op_Minus = auto()  # -
-    Op_Add = auto()  # +
     Kw_IfDef = auto()  # ifdef
     Kw_IfNDef = auto()  # ifndef
     Kw_Define = auto()  # define
@@ -81,6 +79,13 @@ keywords = {
     'true': TokenKind.Kw_True,
     'false': TokenKind.Kw_False
 }
+
+
+class NumberType(Enum):
+    Decimal = auto()
+    Octal = auto()
+    Hexadecimal = auto()
+    Float = auto()
 
 
 class LexerException(Exception):
@@ -108,7 +113,32 @@ def is_identifier_char(c):
     return c == '_' or is_digit(c) or is_letter(c)
 
 
-class Lexer:
+def is_hex_digit(c):
+    return '0' <= c <= '9' or 'A' <= c <= "F" or 'a' <= c <= 'f'
+
+
+# noinspection PyPropertyDefinition
+class ILexer:
+    # 至多前瞻一个token
+    # 前瞻的token被缓存，反复调用lookahead只会返回这个缓存
+    # 调用一次next_token后这个缓存被清空
+    @property
+    def lookahead(self):
+        pass
+
+    def next_token_of_kind(self, kind: TokenKind):
+        pass
+
+    @property
+    def next_identifier(self):
+        pass
+
+    @property
+    def next_token(self) -> tuple:
+        pass
+
+
+class Lexer(ILexer):
     def __init__(self, chunk: str):
         self.chunk = chunk  # source code
         self.line = 1  # current line number
@@ -117,9 +147,6 @@ class Lexer:
         self.cached_lookahead = tuple()
         self.pointer = 0
 
-    # 至多前瞻一个token
-    # 前瞻的token被缓存，反复调用lookahead只会返回这个缓存
-    # 调用一次next_token后这个缓存被清空
     @property
     def lookahead(self):
         if not self.cached_lookahead:
@@ -140,13 +167,13 @@ class Lexer:
 
         def skip():
             while self.not_eof:
-                if self.test('//'):
+                if self.test_str('//'):
                     self.advance(2)
                     while self.not_eof and not is_newline(self.char):
                         self.advance(1)
-                elif self.test('/*'):
+                elif self.test_str('/*'):
                     self.advance(2)
-                    while not self.test('*/'):
+                    while not self.test_str('*/'):
                         # 注释会包含换行
                         # 另一种方法是解析出来，然后数一下有多少换行
                         if not self.newline():
@@ -161,7 +188,7 @@ class Lexer:
                 else:
                     break
 
-        def Token(kind: TokenKind, lexeme: str = ''):
+        def Token(kind: TokenKind, lexeme=None):
             column = self.last_column
             self.last_column = self.column
             return (self.line, column), kind, lexeme
@@ -229,14 +256,19 @@ case ':':
         else:
             if self.newline():
                 return Token(TokenKind.Newline)
-            if self.test('L\"'):
+            if self.test_str('L\"'):
                 self.advance(1)
                 # TODO 宽字符串如何处理
                 s = self.scan_string()
                 return Token(TokenKind.String, s)
             if c == '.' or is_digit(c):
-                # TODO
-                pass
+                t, v = self.scan_number()
+                if t == NumberType.Decimal or t == NumberType.Hexadecimal or t == NumberType.Octal:
+                    return Token(TokenKind.Int, v)
+                elif t == NumberType.Float:
+                    return Token(TokenKind.Float, v)
+                else:
+                    raise Exception("unreachable")
             if c == '_' or is_letter(c):
                 identifier = self.scan_identifier()
                 assert identifier.isidentifier()
@@ -251,7 +283,7 @@ case ':':
     # 返回是否吃进换行符，如果是，更新行号和列号
     # 返回False的情况下，newline对当前字符什么都不做，交给调用者处理
     def newline(self):
-        if self.test('\r\n') or self.test('\n\r'):
+        if self.test_str('\r\n') or self.test_str('\n\r'):
             self.advance(2)
             self.line += 1
             self.column = 0
@@ -273,9 +305,39 @@ case ':':
     def not_eof(self):
         return self.pointer < len(self.chunk)
 
+    # test类函数会检查eof，如果因为遇到eof，也是返回false
+    # 下面的test类函数不必调用test-base，避免小调用，但是应该参考这个
+    def test_base(self, predicate):
+        return self.not_eof and predicate()
+
+    def try_advance_base(self, test, n):
+        res = test()
+        if res:
+            self.advance(n)
+        return res
+
+    def test_char(self, c):
+        return self.not_eof and self.char == c
+
     # 测试chunk接下来是否是s，类似于re.match
-    def test(self, s: str):
+    def test_str(self, s: str):
         return self.chunk.startswith(s, self.pointer)
+
+    def test_charset(self, charset):
+        return self.not_eof and self.char in charset
+
+    def test_predicate(self, predicate):
+        return self.not_eof and predicate(self.char)
+
+    def try_advance_char(self, c):
+        return self.try_advance_base(lambda: self.test_char(c), 1)
+
+    def try_advance_str(self, s: str):
+        return self.try_advance_base(lambda: self.test_str(s), len(s))
+
+    # 自己决定charset是str还是set
+    def try_advance_charset(self, charset):
+        return self.try_advance_base(lambda: self.test_charset(charset), 1)
 
     # 指针前进
     # 不要叫next，避免和python-generator的next冲突
@@ -300,8 +362,64 @@ case ':':
 
     # HARD 想想办法逃课把。。。
     # 首字母无法判断是int还是float
+    # 返回Token
     def scan_number(self):
-        pass
+        pointer = self.pointer
+
+        def lexeme():
+            return self.chunk[pointer: self.pointer]
+
+        # 转为python都是int，所以后缀信息没用
+        # 而且实测int函数不能解析后缀，所以要先解析再advance
+        def abandon_int_suffix():
+            self.try_advance_str('ll')
+            self.try_advance_str('LL')
+            self.try_advance_charset(set('lLuU'))
+
+        def exponent_part():
+            if self.not_eof and self.try_advance_charset('eE'):
+                self.try_advance_charset('+-')
+                while not self.not_eof and is_digit(c):
+                    self.advance(1)
+
+        c = self.char
+        self.advance(1)
+        if c == '.':  # float
+            pass
+        elif c == '0':  # oct or hex
+            c = self.char
+            if self.try_advance_charset('xX'):  # hex
+                if not self.test_predicate(is_hex_digit):
+                    raise LexerException("unfinished hex int")
+                while self.test_predicate(is_hex_digit):
+                    self.advance(1)
+                value = int(lexeme(), 16)
+                abandon_int_suffix()
+                return NumberType.Hexadecimal, value
+            else:  # oct
+                while self.not_eof and '0' <= self.char <= '7':
+                    self.advance(1)
+                value = int(lexeme(), 8)
+                abandon_int_suffix()
+                return NumberType.Octal, value
+        else:  # int or float
+            assert '1' <= c <= '9'
+            # 不管是int还是float，首先解析出整数
+            while self.not_eof and is_digit(self.char):
+                self.advance(1)
+            else:  # while else
+                if self.not_eof and self.test_charset('.eE'):  # float
+                    if self.not_eof and self.try_advance_char('.'):
+                        while not self.not_eof and is_digit(c):
+                            self.advance(1)
+                    exponent_part()
+                    value = float(lexeme())
+                    self.try_advance_charset('fF')
+                    return NumberType.Float, value
+                else:  # int
+                    value = int(lexeme())
+                    abandon_int_suffix()
+                    return NumberType.Decimal, value
 
     # 扫描，完成转移，返回没有两端引号的文本内容
     def scan_string(self):
@@ -349,34 +467,34 @@ case ':':
                 string_builder.append('\\')
                 continue
             comment = '''
-case '0', '1', '2', '3', '4', '5', '6', '7', '8', '9': // \ddd
-    if found := reDecEscapeSeq.FindString(str); found != "" {
-        d, _ := strconv.ParseInt(found[1:], 10, 32)
-        if d <= 0xFF {
+    case '0', '1', '2', '3', '4', '5', '6', '7', '8', '9': // \ddd
+        if found := reDecEscapeSeq.FindString(str); found != "" {
+            d, _ := strconv.ParseInt(found[1:], 10, 32)
+            if d <= 0xFF {
+                buf.WriteByte(byte(d))
+                str = str[len(found):]
+                continue
+            }
+            self.error("decimal escape too large near '%s'", found)
+        }
+    case 'x': // \\xXX
+        if found := reHexEscapeSeq.FindString(str); found != "" {
+            d, _ := strconv.ParseInt(found[2:], 16, 32)
             buf.WriteByte(byte(d))
             str = str[len(found):]
             continue
         }
-        self.error("decimal escape too large near '%s'", found)
-    }
-case 'x': // \\xXX
-    if found := reHexEscapeSeq.FindString(str); found != "" {
-        d, _ := strconv.ParseInt(found[2:], 16, 32)
-        buf.WriteByte(byte(d))
-        str = str[len(found):]
-        continue
-    }
-case 'u': // \\u{XXX}
-    if found := reUnicodeEscapeSeq.FindString(str); found != "" {
-        d, err := strconv.ParseInt(found[3:len(found)-1], 16, 32)
-        if err == nil && d <= 0x10FFFF {
-            buf.WriteRune(rune(d))
-            str = str[len(found):]
-            continue
-        }
-        self.error("UTF-8 value too large near '%s'", found)
-    }                                
-                '''
+    case 'u': // \\u{XXX}
+        if found := reUnicodeEscapeSeq.FindString(str); found != "" {
+            d, err := strconv.ParseInt(found[3:len(found)-1], 16, 32)
+            if err == nil && d <= 0x10FFFF {
+                buf.WriteRune(rune(d))
+                str = str[len(found):]
+                continue
+            }
+            self.error("UTF-8 value too large near '%s'", found)
+        }                                
+                    '''
             self.error("invalid escape sequence near '\\%s'" % c)
         if self.eof or self.char != '\"':
             self.error("unfinished string")
@@ -390,9 +508,9 @@ case 'u': // \\u{XXX}
 
 
 def t(s):
-    l = Lexer(s)
+    lexer = Lexer(s)
     while True:
-        a = l.next_token
+        a = lexer.next_token
         print(a)
         _, k, _ = a
         if k == TokenKind.Eof:
@@ -403,4 +521,5 @@ def tf(f):
     t(readall(f))
 
 
-tf('test_data/skip.cpp')
+tf('test_data/lexer/skip.cpp')
+tf('test_data/define.cpp')
